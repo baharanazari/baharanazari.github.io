8734772659ad67a58ac70f3315fa1e779f75d738
baharanazari <157067291+baharanazari@users.noreply.github.com>
2024-03-12 17:46:40 -0400

    making the varible

--- Lab4/main.js
+++ Lab4/main.js
@@ -9,11 +9,37 @@ function randomValueFromArray(array){
 
 
 
- var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
- var insertX =  ["Willy the Goblin","Big Daddy", "Father Christmas"]
+var insertX =  ["Willy the Goblin","Big Daddy", "Father Christmas"]
 var insertY = ["the soup kitchen","Disneyland","the White House"]
 var insertZ = ["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and crawled away"]
 
 
+randomize.addEventListener('click', result);
 
+function result() {
+
+    var newStory = storyText
+    var xItem = randomValueFromArray(insertX);
+    var yItem = randomValueFromArray(insertY);
+    var zItem = randomValueFromArray(insertZ);
+
+ newStory = newStory.replace(':inserX:',xItem)
+ newStory= newStory.replace(':insertY:',yItem)
+ newStory = newStory.replace(':insertZ:',zItem)
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
