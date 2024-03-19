24b060efcf3efa043cc7fa179a41e22cfcc4eb49
baharanazari <157067291+baharanazari@users.noreply.github.com>
2024-03-19 00:04:38 -0400

    fixing the last part of it

--- Lab4/Part2_Assignment4/main.js
+++ Lab4/Part2_Assignment4/main.js
@@ -5,23 +5,28 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ["pic1.jpg","pic2.jpg","pic3.jpg","pi4.jpg","pic5.jpg"]
+const images = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
 const altText = {
-    "pic1.jpg":"thumbnail image1",
-    "pic2.jpg":"thumbnail image2",
-    "pic3.jpg":"thumbnail image3",
-    "pic4.jpg":"thumbnail image4",
-    "pic5.jpg":"thumbnail image5",
+    "pic1.jpg":"human eye",
+    "pic2.jpg":"A Rock",
+    "pic3.jpg":"half white and half purple flower",
+    "pic4.jpg":"A drawing",
+    "pic5.jpg":"butterfly",
 };
 
 /* Looping through images */
-images.forEach(pic1.jpg)
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+images.forEach(function(image) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image);
+    newImage.setAttribute('alt', altText[image]); // Set alt text based on the filename
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}); 
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', function() {
