15f9b1a5ecfd9b14363e07056e6976979cc66777
baharanazari <157067291+baharanazari@users.noreply.github.com>
2024-03-19 16:24:52 -0400

    creating a test ball constant

--- Lab4/Part3_assignmnet4/main.js
+++ Lab4/Part3_assignmnet4/main.js
@@ -1,5 +1,4 @@
 // setup canvas
-
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -7,16 +6,15 @@ const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
 // function to generate random number
-
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // function to generate random color
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -26,13 +24,14 @@ class Ball {
     this.color = color;
     this.size = size;
   }
+}
+  
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
-  }
-  
-}
+  } 
+  const testBall = new Ball(50, 100, 4, 4, "blue", 10);
 
 
