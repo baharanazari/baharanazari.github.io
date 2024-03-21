4d823d4494714afc3a99c2c006d95072d54efdad
baharanazari <157067291+baharanazari@users.noreply.github.com>
2024-03-20 21:48:29 -0400

    adding commemt to the html and js

--- Lab4/Part2_Assignment4/main.js
+++ Lab4/Part2_Assignment4/main.js
@@ -1,35 +1,41 @@
+// Find the image that is currently displayed
 const displayedImage = document.querySelector('.displayed-img');
+
+// Find the container for thumbnail images
 const thumbBar = document.querySelector('.thumb-bar');
 
+// Find the button element
 const btn = document.querySelector('button');
+
+// Find the overlay element
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-const images = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"]
+/* List of image filenames */
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
-/* Declaring the alternative text for each image file */
+/* Description for each image */
 const altText = {
-    "pic1.jpg":"human eye",
-    "pic2.jpg":"A Rock",
-    "pic3.jpg":"half white and half purple flower",
-    "pic4.jpg":"A drawing",
-    "pic5.jpg":"butterfly",
+    "pic1.jpg": "human eye",
+    "pic2.jpg": "A Rock",
+    "pic3.jpg": "half white and half purple flower",
+    "pic4.jpg": "A drawing",
+    "pic5.jpg": "butterfly",
 };
 
-/* Looping through images */
-images.forEach(function(image) {
+/* Loop through each image */
+images.forEach(function (image) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', image);
-    newImage.setAttribute('alt', altText[image]); // Set alt text based on the filename
+    newImage.setAttribute('alt', altText[image]); // Set alternative text based on the image filename
     thumbBar.appendChild(newImage);
     newImage.addEventListener('click', e => {
         displayedImage.src = e.target.src;
         displayedImage.alt = e.target.alt;
     });
-}); 
+});
 
-/* Wiring up the Darken/Lighten button */
-btn.addEventListener('click', function() {
+/* Add event listener to the button to darken or lighten the overlay */
+btn.addEventListener('click', function () {
     // Check the current class name set on the button
     const currentClass = btn.getAttribute('class');
 
@@ -44,3 +50,8 @@ btn.addEventListener('click', function() {
         overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
     }
 });
+
+
+
+
+
